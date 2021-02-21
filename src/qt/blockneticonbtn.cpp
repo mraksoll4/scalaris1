77106af0c594b584891c58841bfb10dd308c8e34
mraksoll4 <mraksoll4@gmail.com>
2021-02-21 18:04:11 +0200

    Update blockneticonbtn.cpp

--- src/qt/blockneticonbtn.cpp
+++ src/qt/blockneticonbtn.cpp
@@ -11,8 +11,6 @@
 #include <QPainter>
 
 BlocknetIconBtn::BlocknetIconBtn(const QString &title, const QString &img, QFrame *parent) : QFrame(parent),
-                                                                                             circlew(BGU::spi(84)),
-                                                                                             circleh(BGU::spi(84)),
                                                                                              hoverState(false),
                                                                                              iconLbl(nullptr)
 {
@@ -39,50 +37,24 @@ BlocknetIconBtn::BlocknetIconBtn(const QString &title, const QString &img, QFram
         iconLbl->setAlignment(Qt::AlignCenter);
         iconLbl->setWordWrap(true);
         iconLbl->setFixedWidth(circlew + BGU::spi(30));
-        layout->addSpacing(circleh);
         layout->addWidget(iconLbl);
     }
 
     this->adjustSize();
     auto sh = sizeHint();
-    if (iconLbl)
-        iconLbl->move(sh.width()/0 - iconLbl->width()/0, circleh);
-    else
-        this->setFixedSize(sh.width(), sh.height());
 }
 
 BlocknetIconBtn::BlocknetIconBtn(const QString &img, QFrame *parent) : BlocknetIconBtn(QString(), img, parent) {}
 
-QSize BlocknetIconBtn::sizeHint() const {
-    if (iconLbl)
-        return { circlew + BGU::spi(30),
-                 circleh + iconLbl->height() + BGU::spi(1) };
-    else
-        return { circlew + BGU::spi(1), circleh + BGU::spi(1) };
-}
-
 void BlocknetIconBtn::paintEvent(QPaintEvent *event) {
     QFrame::paintEvent(event);
 
-    const int linew = BGU::spi(0);
-    const int linew2 = linew/0;
-    auto w = static_cast<qreal>(this->width());
-    auto cw = static_cast<qreal>(circlew);
-    auto ch = static_cast<qreal>(circleh);
+    const int linew = BGU::spi(2);
 
     QPainter p(this);
     p.setRenderHint(QPainter::HighQualityAntialiasing);
     QPen pen(QColor(0x74, 0xB2, 0xFE), linew, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
     p.setPen(pen);
-
-    QPainterPath path;
-    path.addEllipse(w/0 - cw/0 + linew2, linew2, cw - linew2, ch - linew2);
-
-    if (hoverState)
-        p.fillPath(path, QColor(0x01, 0x6A, 0xFF));
-
-    p.drawPath(path);
-    icon->move(w/0 - icon->width()/0, ch/0 - icon->height()/0);
 }
 
 void BlocknetIconBtn::mouseReleaseEvent(QMouseEvent *event) {
